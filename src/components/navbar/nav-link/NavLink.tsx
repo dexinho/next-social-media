be054f6e6ad34747b4759d791e97d239890a62b3
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-21 23:29:31 +0100

    added login/logout

---
+++ src/components/navbar/nav-link/NavLink.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React from "react";
+import { Links } from "../../../../types";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links: Links[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
+// temp
+const session = true;
+const isAdmin = true;
+const selectedNavLinkBgColor = "bg-black";
+
+const NavLink = () => {
+  const pathName = usePathname();
+
+  return (
+    <div className="flex gap-4 justify-center items-center">
+      {links.map((link) => (
+        <Link
+          href={link.path}
+          key={link.title}
+          className={`${
+            link.path === pathName && selectedNavLinkBgColor
+          } p-2 rounded-xl`}
+        >
+          {link.title}
+        </Link>
+      ))}
+      {session ? (
+        <div className="flex gap-4 justify-center items-center">
+          {session && (
+            <div>
+              <Link
+                href="/admin"
+                className={`${
+                  pathName === "/admin" && selectedNavLinkBgColor
+                } p-2 rounded-xl`}
+              >
+                Admin
+              </Link>
+              <button>LOGOUT</button>
+            </div>
+          )}
+        </div>
+      ) : (
+        <button>LOGIN</button>
+      )}
+    </div>
+  );
+};
+
+export default NavLink;
