178a515eb538f5e4b852f66f2a00395b294817cb
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-23 18:13:55 +0100

    added bavvar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,47 @@
-import React from "react";
+"use client";
 
-const HomePage = () => {
-  new Promise<void>((res) => {
-    setTimeout(() => {
-      res();
-    }, 1000);
-  });
-  return <div>Home Page</div>;
-};
+import { useEffect, useState } from "react";
+import { ToDo } from "../../types";
 
-export default HomePage;
+export default function Home() {
+  const [todos, setTodos] = useState<ToDo[]>([]);
+
+  useEffect(() => {
+    async function fetchTodos() {
+      const response = await fetch(`/api/todos`);
+
+      console.log(response)
+    }
+
+    fetchTodos();
+  }, []);
+
+  return (
+    <div className="flex flex-col w-full justify-center items-center min-h-screen">
+      List Page
+      <div className="flex flex-col w-10/12 bg-gray-900 p-4 rounded-md shadow-2xl my-4">
+        <div className="w-full flex justify-between px-4 opacity-50 mb-2">
+          <div>Title</div>
+          <div>Category</div>
+          <div>Status</div>
+        </div>
+        {todos.map((todo, idx) => (
+          <>
+            <div
+              className={`${
+                idx === 0
+                  ? "rounded-t-md"
+                  : todos.length - 1 === idx
+                  ? "rounded-b-md"
+                  : ""
+              } bg-gray-800 hover:bg-gray-700 cursor-pointer shadow-md flex w-full justify-between p-4`}
+            >
+              <div>{todo.id}</div>
+              <div>{todo.isCompleted ? "Completed" : "Not Completed"}</div>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+}
